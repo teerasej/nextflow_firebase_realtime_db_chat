cbc8725612cd70b1d9e04a89e1a4fd242ef8a983
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-01-12 22:20:31 +0700

    init firebase to test stream

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,8 +9,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  FirebaseDatabase database = FirebaseDatabase.instance;
   @override
   Widget build(BuildContext context) {
+    DatabaseReference ref =
+        FirebaseDatabase.instance.ref("dev/auction_sequence");
+    var stream = ref.onValue;
+    stream.listen((DatabaseEvent event) {
+      print('Event Type: ${event.type}');
+      print('Snapshot: ${event.snapshot}');
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Auction'),
